7406926dd53b4c465d24f046a5e37c30e3946a17
abnerjp <abner.pelisser@gmail.com>
2021-02-04 14:23:09 -0300

    Adicionado script para criação de slides e adicionado slide na página de Portifólio

---
+++ js/script.js
@@ -0,0 +1,14 @@
+if (window.SimpleSlide) {
+  new SimpleSlide({
+    slide: "quote",
+    auto: true,
+    time: 5000,
+  });
+  
+  new SimpleSlide({
+    slide: "portifolio",
+    auto: true,
+    time: 10000,
+    nav: true
+  });
+}
